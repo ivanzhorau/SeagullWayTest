ff228906b2e24bbb1d79734c90ecb4642e2a4350
Ivan <ivanzhuraulievich@gmail.com>
2024-08-16 03:46:00 +0400

    Add SignInByEmailPage

---
+++ src/main/java/by/goodreads/pages/SignInByEmailPage.java
@@ -0,0 +1,25 @@
+package by.goodreads.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SignInByEmailPage extends Page{
+    private static final By emailInput = By.cssSelector("#ap_email");
+    private static final By passwordInput = By.cssSelector("#ap_password");
+    private static final By signInButton = By.cssSelector("#signInSubmit");
+
+    public SignInByEmailPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void enterEmail(String email){
+        getDriver().findElement(emailInput).sendKeys(email);
+    }
+    public void enterPassword(String password){
+        getDriver().findElement(passwordInput).sendKeys(password);
+    }
+
+    public void signInClick(){
+        getDriver().findElement(signInButton).click();
+    }
+}
