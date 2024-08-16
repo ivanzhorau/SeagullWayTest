c33f65ed28fe063f2cf13d3b19095f25e1d4ebc9
Ivan <ivanzhuraulievich@gmail.com>
2024-08-16 04:31:32 +0400

    Rename HomePage to StartPage

--- src/main/java/by/goodreads/pages/StartPage.java
+++ src/main/java/by/goodreads/pages/StartPage.java
@@ -3,11 +3,11 @@ package by.goodreads.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class HomePage extends Page {
+public class StartPage extends Page {
     private static final By signUpButton = By.cssSelector("#newAccountBox a.gr-button:nth-child(3)");
     private static final By signInButton = By.cssSelector("#signIn a:nth-child(1)");
 
-    public HomePage(WebDriver driver) {
+    public StartPage(WebDriver driver) {
         super(driver);
     }
 
