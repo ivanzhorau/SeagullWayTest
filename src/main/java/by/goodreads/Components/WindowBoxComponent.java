c06e5a86dd27f888dfef75b4cac007f52f6c6ce7
Ivan <ivanzhuraulievich@gmail.com>
2024-08-16 08:05:30 +0400

    Add WindowBoxComponent

---
+++ src/main/java/by/goodreads/Components/WindowBoxComponent.java
@@ -0,0 +1,35 @@
+package by.goodreads.Components;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.FluentWait;
+
+import java.time.Duration;
+
+public class WindowBoxComponent extends BasicComponent{
+    private static final By rateComponent = By.cssSelector(".stars");
+    private static final By startReadingDateComponent = By.cssSelector(".startedAtDatePickers");
+    private static final By endReadingDateComponent = By.cssSelector(".endedAtDatePickers");
+    private static final By postButton = By.cssSelector("input[id *= 'review_submit']");
+
+    public WindowBoxComponent(WebElement parentElement) {
+        super(parentElement);
+    }
+
+    public RateComponent getRateComponent(){
+        return new RateComponent(getParentElement().findElement(rateComponent));
+    }
+
+    public DatePickerComponent getStartReadingDateComponent(){
+        return new DatePickerComponent(getParentElement().findElement(startReadingDateComponent));
+    }
+    public DatePickerComponent getEndReadingDateComponent(){
+        return new DatePickerComponent(getParentElement().findElement(endReadingDateComponent));
+    }
+    public void waitForLoad(){
+        new FluentWait<>(getParentElement()).withTimeout(Duration.ofSeconds(5)).until(webElement -> !webElement.findElements(By.cssSelector(".bookTitle")).isEmpty());
+    }
+    public void clickPost(){
+        getParentElement().findElement(postButton).click();
+    }
+}
--- src/main/java/by/goodreads/pages/BasicHomePage.java
+++ src/main/java/by/goodreads/pages/BasicHomePage.java
@@ -1,12 +1,13 @@
 package by.goodreads.pages;
 
+import by.goodreads.Components.WindowBoxComponent;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class BasicHomePage extends Page{
     private static final By searchBoxInput = By.cssSelector(".searchBox__input--navbar");
     private static final By searchBoxButton = By.cssSelector(".searchBox__icon--navbar");
-
+    private static final By windowBox = By.cssSelector("#box");
     public BasicHomePage(WebDriver driver) {
         super(driver);
     }
@@ -17,5 +18,9 @@ public class BasicHomePage extends Page{
 
     public void clickSearchButton(){
         getDriver().findElement(searchBoxButton).click();
+    }
+
+    public WindowBoxComponent getWindowBox(){
+        return new WindowBoxComponent(getDriver().findElement(windowBox));
     }
 }
