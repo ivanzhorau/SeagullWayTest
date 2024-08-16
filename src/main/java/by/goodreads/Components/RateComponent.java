3fc56e5f8869cae2ccaf643bf75ddb3a03ff8b07
Ivan <ivanzhuraulievich@gmail.com>
2024-08-16 06:21:03 +0400

    Add RateComponent

---
+++ src/main/java/by/goodreads/Components/RateComponent.java
@@ -0,0 +1,15 @@
+package by.goodreads.Components;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class RateComponent extends BasicComponent{
+    private static final By star = By.cssSelector("a");
+    public RateComponent(WebElement parentElement) {
+        super(parentElement);
+    }
+
+    public void setRate(int rate){
+        getParentElement().findElements(star).get(rate-1).click();
+    }
+}
