d529c124cb19d794929809ed99f91863858226d5
Ivan <ivanzhuraulievich@gmail.com>
2024-08-16 05:28:50 +0400

    Add TableItemComponent

---
+++ src/main/java/by/goodreads/Components/TableItemComponent.java
@@ -0,0 +1,10 @@
+package by.goodreads.Components;
+
+import org.openqa.selenium.WebElement;
+
+public class TableItemComponent extends BasicComponent{
+
+    public TableItemComponent(WebElement parentElement) {
+        super(parentElement);
+    }
+}
