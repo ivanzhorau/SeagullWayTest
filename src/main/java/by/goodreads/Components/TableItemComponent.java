1ebdec8e5b20533625b548ff1f5b6f5d30cd61e4
Ivan <ivanzhuraulievich@gmail.com>
2024-08-16 06:12:12 +0400

    Update TableItemComponent

--- src/main/java/by/goodreads/Components/TableItemComponent.java
+++ src/main/java/by/goodreads/Components/TableItemComponent.java
@@ -1,10 +1,23 @@
 package by.goodreads.Components;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-public class TableItemComponent extends BasicComponent{
+public class TableItemComponent extends BasicComponent {
+    private static final By wantToReadButton = By.cssSelector("button.wtrToRead");
+    private static final By readButton = By.cssSelector(".wtrExclusiveShelves li[data-shelf-name='read'] button");
+    private static final By openShelfButton = By.cssSelector("button.wtrShelfButton");
 
     public TableItemComponent(WebElement parentElement) {
         super(parentElement);
+    }
+
+    public void clickWantToRead(){
+        getParentElement().findElement(wantToReadButton).click();
+    }
+
+    public void markAsRead(){
+        getParentElement().findElement(openShelfButton).click();
+        getParentElement().findElement(readButton).click();
     }
 }
