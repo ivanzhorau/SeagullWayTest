1880e50d764002e659f96cf72fc4951a2692e3c7
Ivan <ivanzhuraulievich@gmail.com>
2024-08-16 05:29:02 +0400

    Add SearchPage

---
+++ src/main/java/by/goodreads/pages/SearchPage.java
@@ -0,0 +1,19 @@
+package by.goodreads.pages;
+
+import by.goodreads.Components.TableItemComponent;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import java.util.List;
+
+public class SearchPage extends BasicHomePage {
+    private static final By tableItem = By.cssSelector(".tableList > tbody > tr");
+
+    public SearchPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public List<TableItemComponent> getTableItems() {
+        return getDriver().findElements(tableItem).stream().map(TableItemComponent::new).toList();
+    }
+}
