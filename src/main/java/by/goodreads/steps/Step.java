0122b942e8f173f3b8c13ec332e85789b1b186c3
Ivan <ivanzhuraulievich@gmail.com>
2024-08-19 09:40:11 +0400

    Update Step

--- src/main/java/by/goodreads/steps/Step.java
+++ src/main/java/by/goodreads/steps/Step.java
@@ -2,13 +2,41 @@ package by.goodreads.steps;
 
 public abstract class Step {
 
+    private Role role = Role.ACTION;
+
     public abstract void run();
 
-    public void runAsCheck() {
+    public void execute() throws Throwable {
         try {
+            System.out.printf("Step: '%s' started%n", getSummary());
             run();
+            System.out.printf("Step: '%s' completed successfully%n", getSummary());
         } catch (Throwable e) {
-            e.printStackTrace();
+            System.out.printf("Step: '%s' failed%n", getSummary());
+            role.process(e);
         }
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getSummary(){
+        return this.getClass().getName();
+    }
+    public enum Role{
+        ACTION {
+            @Override
+            public void process(Throwable e) throws Throwable {
+                throw e;
+            }
+        },
+        CHECK{
+            @Override
+            public void process(Throwable e) {
+                e.printStackTrace();
+            }
+        };
+        public abstract void process(Throwable e) throws Throwable;
     }
 }
